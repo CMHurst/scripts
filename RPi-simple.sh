afed9e4b3a86f3595b78ca9d3250909802da42be
CHurst <christianh6064@yahoo.com>
2022-04-21 13:19:30 -0500

    Update RPi-simple.sh

--- RPi-simple.sh
+++ RPi-simple.sh
@@ -6,8 +6,8 @@ read -n1 -p "Would you like to install ZeroTier? [y, N]" zt
 #sudo apt-get update && apt-get upgrade -y && apt-get auto-remove -y
 
 case $zt in
-    y|Y echo true ;;
-    n|N echo false ;;
+    y|Y) echo "true" ;;
+    n|N) echo "false" ;;
     *) echo invalid ;;
 
 esac
