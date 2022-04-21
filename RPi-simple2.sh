6cda2b0595f0eac2ec11641fc74025f8c41a6ade
CHurst <christianh6064@yahoo.com>
2022-04-21 13:12:11 -0500

    Create RPi-simple2.sh

---
+++ RPi-simple2.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+read -n1 -p "Would you like to install ZeroTier? [y, N]" zt
+
+
+sudo apt-get update && apt-get upgrade -y && apt-get auto-remove -y
+
+if [[ $zt == "Y" || $zt == "y" ]]; then
+    echo "true"
+else
+    echo "false"
+
