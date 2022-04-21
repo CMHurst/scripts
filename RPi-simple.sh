f22069e20223824a119d76a0c5b6b7861185aaf2
CHurst <christianh6064@yahoo.com>
2022-04-21 12:49:52 -0500

    Update RPi-simple.sh

--- RPi-simple.sh
+++ RPi-simple.sh
@@ -5,7 +5,7 @@ read input
 
 if input == 'y' | input == 'Y' # Updates, upgrades, and cleans repos
 then
-    echo -n "\y\y\y") | sudo apt update && apt upgrade && apt auto-remove
+    echo -n "\y\y\y" | sudo apt update && apt upgrade && apt auto-remove
 else
     echo "Skipping updates"
 fi
