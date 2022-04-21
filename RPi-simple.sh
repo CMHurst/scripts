df1acf56dad96825e752b90cd633442c3ba81c53
CHurst <christianh6064@yahoo.com>
2022-04-21 12:49:28 -0500

    Update RPi-simple.sh

--- RPi-simple.sh
+++ RPi-simple.sh
@@ -3,11 +3,11 @@
 echo "Would you like to update, upgrade and clean local repositories?"
 read input
 
-if (input == 'y' | input == 'Y') # Updates, upgrades, and cleans repos
+if input == 'y' | input == 'Y' # Updates, upgrades, and cleans repos
 then
     echo -n "\y\y\y") | sudo apt update && apt upgrade && apt auto-remove
 else
-    printf("Skipping updates")
+    echo "Skipping updates"
 fi
 
 
