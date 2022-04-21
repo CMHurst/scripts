48e9ee0cfd9f8c8095a7fcf69a3aa395f63a4651
CHurst <christianh6064@yahoo.com>
2022-04-21 12:08:19 -0500

    Update RPi-install.sh

--- RPi-install.sh
+++ RPi-install.sh
@@ -1,7 +1,36 @@
 #! /usr/bin/bash
 promptResponses = ""
-promptResponses += "y"
-promptResponses += "y"
+zt = 0
+ztpersonal = 0
 
-printf promptResponses | sudo apt update && apt upgrade
+printf ("\nThis script will automatically update, upgrade, and clean repositories; in addition to installing software if desired",\n\r)
+
+printf("Would you like to install ZeroTier?")
+read zt
+
+if zt
+then
+    printf(\n, "Would you like to connect to personal ZeroTier group?")
+    read ztpersonal
+fi
+    print(\n, "Zerotier skipped", \n)
+
+printf()
+
+printf("y\y\y") | sudo apt update && apt upgrade && apt auto-remove
+
+
+if zt
+then # Install ZeroTier
+    curl -s https://install.zerotier.com | sudo bash
+    sudo systemctl enable zerotier-one
+
+    if ztpersonal # Connect to Personal network
+        sudo zerotier-cli join 3efa5cb78a19db4e
+        printf(\n,"Personal Zerotier network joined:")
+        sudo zerotier-cli listnetworks
+        printf(\n)
+
+    
+    
 
