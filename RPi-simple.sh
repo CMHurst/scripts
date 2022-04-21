3c49bffde864fc310a8e8dbaaaaa3321df3762f6
CHurst <christianh6064@yahoo.com>
2022-04-21 13:14:04 -0500

    update

--- RPi-simple.sh
+++ RPi-simple.sh
@@ -3,10 +3,11 @@
 read -n1 -p "Would you like to install ZeroTier? [y, N]" zt
 
 
-sudo apt-get update && apt-get upgrade -y && apt-get auto-remove -y
+#sudo apt-get update && apt-get upgrade -y && apt-get auto-remove -y
 
 case $zt in
     y|Y echo true ;;
     n|N echo false ;;
-    *) echo invalid
+    *) echo invalid ;;
 
+esac
